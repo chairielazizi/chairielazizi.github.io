a838392b09468396bf7cf26b056d4085aa6e4ad5
chairielazizi <azizihr2000@gmail.com>
2024-01-29 11:26:39 +0800

    banner component mark as done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Image from '../assets/avatar.svg';
+import Image from '../assets/riel1.png';
 import {FaGithub, FaYoutube, FaDribbble} from 'react-icons/fa';
 import {TypeAnimation} from 'react-type-animation';
 import {motion} from 'framer-motion';
@@ -8,36 +8,45 @@ import {fadeIn} from '../variants';
 
 const Banner = () => {
   return (
-    <div id='home' className='section'>
+    <section id='home' className='min-h-[85vh] lg:min-h-[78vh] flex items-center'>
       <div className='container mx-auto'>
-        <div>
-          <h1>
-            CHAIRIEL <span>AZIZI</span>
-          </h1>
-          <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
-            <span className='text-whit mr-4'>I am a</span>
-            <TypeAnimation sequence={['Automation Tester',2000, 'Developer',2000,'Tinkerer',2000, ]}
-            speed={150}
-            className='text-accent1'
-            wrapper='span'
-            repeat={Infinity}
-            />
+        <div className='flex flex-col gap-y-8 lg:flex-row items-center'>
+          {/* text */}
+          <div className='flex-1 text-center font-secondary lg:text-left'>
+            <motion.h1 variants={fadeIn('up', 0.4)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.7}} className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
+              CHAIRIEL <span>AZIZI</span>
+            </motion.h1>
+            <motion.div variants={fadeIn('up', 0.5)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.7}} className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+              <span className='text-whit mr-4'>I am a</span>
+              <TypeAnimation sequence={['Automation Tester',2000, 'Developer',2000,'Tinkerer',2000, ]}
+              speed={150}
+              className='text-accent1'
+              wrapper='span'
+              repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p variants={fadeIn('up', 0.6)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.7}} className='mb-8 max-w-lg mx-auto lg:mx-0'>
+              Just living life like everybody else do. Love networking, play with raspberry pi and docker, and of course test any applications or website as that what I usually do. Sometimes code. 
+            </motion.p>
+            <motion.div variants={fadeIn('up', 0.7)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.7}} className='flex gap-x-6 items-center mb-12 max-w-max mx-auto lg:mx-0'>
+              <button className='btn btn-lg'>Hit Me Up</button>
+              <a href='#' className='text-gradient1 btn-link'>
+                My Portfolio
+              </a>
+            </motion.div>
+            <motion.div variants={fadeIn('up', 0.7)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.7}} className='flex text-[27px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+              {/* social */}
+              <a href=''><FaGithub/></a>
+              <a href=''><FaYoutube/></a>
+            </motion.div>
           </div>
-          <p>
-            Just living life like everybody else do. Love networking, play with raspberry pi and docker, and of course test any applications or website as that what I usually do. I also code and developing during my free time. And still find any interesting open source software for me to try.
-          </p>
-          <div>
-            <button className='btn btn-lg'>Hit Me Up</button>
-            <a href='#' className='text-gradient btn-link'>
-              My Portfolio
-            </a>
-          </div>
-        </div>
-        <div>
-          {/* <img src={Image} alt='' /> */}
+          {/* image */}
+          <motion.div variants={fadeIn('down', 0.5)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.7}} className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[480px]'>
+            <img src={Image} alt='' className='image'/>
+          </motion.div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-  <header className='py-8'>
+  <header className='py-8 lg:py-6'>
     <div className='container mx-auto'>
       <div className='flex justify-between items-center'>
         <a href='#'>
