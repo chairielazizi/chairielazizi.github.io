9c70f13f8eed1ed7c55e53e3570c60fbe67733fe
chairielazizi <azizihr2000@gmail.com>
2024-02-15 11:22:08 +0800

    work page done

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ const About = () => {
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.3 }}
-            className="flex-1 bg-about1 bg-contain bg-no-repeat h-[440px] mix-blend-lighten bg-top"
+            className="flex-1 bg-about1 bg-contain bg-no-repeat h-[440px] mix-blend-lighten bg-top sm:hidden"
           ></motion.div>
           {/* text */}
           <motion.div
